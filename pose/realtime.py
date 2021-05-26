fa5d7ac3695c9e2056467347ca7e21748e3283b1
joann129 <D0746323@o365.fcu.edu.tw>
2021-05-26 22:48:42 +0800

    舉手開影片成功

--- pose/realtime.py
+++ pose/realtime.py
@@ -9,6 +9,23 @@ import pylint
 from datetime import datetime
 import subprocess
 
+start = 73.66   #肩膀開始的角度
+start1 = 347.2  #手腕開始的角度
+angle = 45      #角度正常範圍
+startcheck = 0       #開始了沒
+
+def calAngle(p1, p2, p3):
+    if p1[0]==0 or p1[1]==0 or p2[0]==0 or p2[1]==0 or p3[0]==0 or p3[1]==0:
+        return -1
+
+    vector1 = [p2[0]-p1[0], p2[1]-p1[1]]
+    vector2 = [p3[0]-p2[0], p3[1]-p2[1]]
+    angle = math.atan2(vector2[1], vector2[0]) - math.atan2(vector1[1], vector1[0])
+    angle = angle/math.pi*180	# change arc to degree
+    if angle < 0:
+        angle= angle + 360
+    return angle
+
 
 try:
     # Import Openpose (Windows/Ubuntu/OSX)
@@ -55,14 +72,13 @@ try:
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    print("Current Time =", current_time)
-
-    # p=subprocess.Popen("python runvideo.py", shell=True)  
+    print("Current Time =", current_time)  
 
     while True:
         ret, frame = cap.read()
         if ret == False:
             print("error")
+            break
 
         img = frame
         datum.cvInputData = img
@@ -71,19 +87,36 @@ try:
         
         openframe = datum.cvOutputData
         count += 1
+
+        #start check
+        tmp = calAngle(datum.poseKeypoints[0][1], datum.poseKeypoints[0][2], datum.poseKeypoints[0][3])
+        tmp1 = calAngle(datum.poseKeypoints[0][2], datum.poseKeypoints[0][3], datum.poseKeypoints[0][4])
+        if not (tmp < start+angle and start-angle < tmp and (tmp1<33 or start1-angle < tmp1)) and not startcheck: #起始動作沒有對齊
+            cv2.putText(openframe, str(count), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 3, cv2.LINE_AA)
+            cv2.imshow("frame", openframe)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            continue;
+        else:
+            if not startcheck:
+                p=subprocess.Popen("python runvideo.py", shell=True)
+                print("start")
+                startcheck = 1 #影片開始
+
+        
+
         cv2.putText(openframe, str(count), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 3, cv2.LINE_AA)
         cv2.imshow("frame", openframe)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            print("Current Time =", current_time)
-
-            print(count)
-            cv2.destroyAllWindows()
             break
 
     # Release
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+    print(count)
     cap.release()
     cv2.destroyAllWindows()
 
@@ -95,5 +128,6 @@ except Exception as e:
     print("Current Time =", current_time)
 
     print(count)
-
+    cap.rlease()
+    cv2.destroyAllWindows()
     sys.exit(-1)
