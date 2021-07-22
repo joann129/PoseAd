6a3fd3fa4de8f3480223250be8d9dcb3386d4495
noyo1018 <56072433+noyo1018@users.noreply.github.com>
2021-07-22 17:29:55 +0800

    成功辨識並且回傳類別結果

--- app/server.py
+++ app/server.py
@@ -1,47 +1,61 @@
-import socket
+from socket import *
+import numpy as np
 import jpysocket
+import cv2
+import base64
+import time
 
-host = '192.168.0.105'  # 對server端為主機位置
-port = 5555
-# host = socket.gethostname()
+def main():
+    HOST = '192.168.0.106'
+    PORT = 5555
+    BUFSIZ = 1024*20
+    ADDR = (HOST, PORT)
+    tcpSerSock = socket(AF_INET, SOCK_STREAM)
+    tcpSerSock.bind(ADDR)
+    tcpSerSock.listen(5)
+    while True:
+        rec_d = bytes([])
+        print('waiting for connection...')
+        tcpCliSock, addr = tcpSerSock.accept()
+        print('...connected from:', addr)
+        tcpCliSock.send(jpysocket.jpyencode('StartSend'))
+        
+        rec = tcpCliSock.recv(1024)
+        rec = jpysocket.jpydecode(rec)
+        
+        print(rec)
+        if rec == "face":
+            while True:
+                data = tcpCliSock.recv(BUFSIZ)
+                if not data or len(data) == 0:
+                    break
+                else:
+                    rec_d = rec_d + data
 
-address = (host, port)
+            path = 'D:/task/d.txt'
+            f = open(path, 'w')
+            f.write(str(rec_d))
+            f.close()
+            
+            with open("D:/task/d.txt","r") as f:
+                img = base64.b64decode(f.read()[1:])
+                print(type(f.read()))
+                fh = open("D:/task/pic_2_sucess22.jpg","wb")
+                fh.write(img)
+                fh.close()
+            time.sleep(1)
+            fa = open("D:/task/rec.txt","r")
+            rec_send = fa.readline()
+            print(rec_send)
+            tcpCliSock.close()
+            tcpCliSock, addr = tcpSerSock.accept()
+            tcpCliSock.send(jpysocket.jpyencode(rec_send))
+            print('send complete')
+            tcpCliSock.close()
+        
+        
+            
+    tcpSerSock.close()
 
-socket01 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# AF_INET:默認IPv4, SOCK_STREAM:TCP
-
-socket01.bind(address)  # 讓這個socket要綁到位址(ip/port)
-socket01.listen(5)  # listen(backlog)
-# backlog:操作系統可以掛起的最大連接數量。該值至少為1，大部分應用程序設為5就可以了
-print('Socket Startup')
-
-conn, addr = socket01.accept()  # 接受遠程計算機的連接請求，建立起與客戶機之間的通信連接
-# 返回（conn,address)
-# conn是新的套接字對象，可以用來接收和發送數據。address是連接客戶端的地址
-print('Connected by', addr)
-
-##################################################
-# 傳送開始傳送的訊號
-
-# 開始接收
-msgsend=jpysocket.jpyencode("StartSend\n")
-conn.send(msgsend)
-buf = conn.recv(1024)
-buf = jpysocket.jpydecode(buf)
-print(buf)
-# conn.send(msgsend)
-print('begin write image file "moonsave.png"')
-imgFile = open('moonsave.png', 'w')  # 開始寫入圖片檔
-while True:
-    imgData = conn.recv(512)  # 接收遠端主機傳來的數據
-    if not imgData:
-        break  # 讀完檔案結束迴圈
-    imgFile.write(imgData)
-imgFile.close()
-print(imgData.decode('utf-8'))
-print('image save')
-##################################################
-
-conn.close()  # 關閉
-socket01.close()
-print('server close')
+if __name__ == "__main__":
+    main()
